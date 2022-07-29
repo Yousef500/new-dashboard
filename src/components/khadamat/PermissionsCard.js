35f66cb9627109607623948b17933a99e46d3df0
Yousef500 <Kazuya14725@gmail.com>
2022-07-29 23:17:25 +0300

    record saving

--- src/components/khadamat/PermissionsCard.js
+++ src/components/khadamat/PermissionsCard.js
@@ -32,27 +32,27 @@ const PermissionsCard = ({
                 background: `linear-gradient(
                         20deg,
                         hsl(180deg 25% 97%) 0%,
-  hsl(177deg 30% 97%) 31%,
-  hsl(174deg 35% 97%) 39%,
-  hsl(170deg 39% 97%) 42%,
-  hsl(167deg 43% 96%) 44%,
-  hsl(163deg 46% 96%) 45%,
-  hsl(159deg 48% 96%) 45%,
-  hsl(155deg 50% 96%) 45%,
-  hsl(150deg 51% 96%) 45%,
-  hsl(145deg 52% 96%) 46%,
-  hsl(139deg 52% 96%) 46%,
-  hsl(131deg 52% 96%) 47%,
-  hsl(123deg 51% 96%) 49%,
-  hsl(113deg 52% 96%) 51%,
-  hsl(104deg 54% 96%) 54%,
-  hsl(95deg 55% 96%) 57%,
-  hsl(88deg 56% 95%) 62%,
-  hsl(80deg 57% 95%) 67%,
-  hsl(73deg 57% 95%) 74%,
-  hsl(67deg 57% 94%) 81%,
-  hsl(60deg 57% 94%) 90%,
-  hsl(54deg 67% 94%) 100%
+                        hsl(177deg 30% 97%) 31%,
+                        hsl(174deg 35% 97%) 39%,
+                        hsl(170deg 39% 97%) 42%,
+                        hsl(167deg 43% 96%) 44%,
+                        hsl(163deg 46% 96%) 45%,
+                        hsl(159deg 48% 96%) 45%,
+                        hsl(155deg 50% 96%) 45%,
+                        hsl(150deg 51% 96%) 45%,
+                        hsl(145deg 52% 96%) 46%,
+                        hsl(139deg 52% 96%) 46%,
+                        hsl(131deg 52% 96%) 47%,
+                        hsl(123deg 51% 96%) 49%,
+                        hsl(113deg 52% 96%) 51%,
+                        hsl(104deg 54% 96%) 54%,
+                        hsl(95deg 55% 96%) 57%,
+                        hsl(88deg 56% 95%) 62%,
+                        hsl(80deg 57% 95%) 67%,
+                        hsl(73deg 57% 95%) 74%,
+                        hsl(67deg 57% 94%) 81%,
+                        hsl(60deg 57% 94%) 90%,
+                        hsl(54deg 67% 94%) 100%
                       )`,
                 height: 500,
             }}
@@ -91,6 +91,7 @@ const PermissionsCard = ({
                     color={actionColor}
                     onClick={handleSubmit}
                     sx={{ fontSize: 20 }}
+                    disabled={!checked.length}
                 >
                     {actionLabel}
                 </MDButton>
